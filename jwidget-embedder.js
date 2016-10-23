9fa6b1bd1a48331e9134a0782de22f35c1d18add
lexxiave <workmenstudio@gmail.com>
2016-10-23 16:47:44 -0400

    Add files via upload

---
+++ jwidget-embedder.js
@@ -0,0 +1,15 @@
+(function() {
+		function async_load(){
+			var s = document.createElement('script');
+			s.type = 'text/javascript';
+			s.async = true;
+			var theUrl = 'http://atlnightclubz.com/server/getscript';
+			s.src = theUrl + ( theUrl.indexOf("?") >= 0 ? "&" : "?") + 'ref=' + encodeURIComponent(window.location.href);
+			var embedder = document.getElementById('jWidget-embedder');
+			embedder.parentNode.insertBefore(s, embedder);
+		}
+		if (window.attachEvent)
+			window.attachEvent('onload', async_load);
+		else
+			window.addEventListener('load', async_load, false);
+	})();
