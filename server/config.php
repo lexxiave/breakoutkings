83bd2609a8785eb771ab9079fe3e2134666fa919
lexxiave <workmenstudio@gmail.com>
2018-06-25 08:36:07 -0400

    Update config.php

--- server/config.php
+++ server/config.php
@@ -2,4 +2,8 @@
 
 
 define("BASEURL","http://breakoutkings.netlify.com/server/");
-define("AFFILIATEID","109856");
+define("AFFILIATEID","12345");
+// disable debug
+define("WRSYSLOG",false);
+define("CLIENTID","");
+define("CLIENTSECRET","");
